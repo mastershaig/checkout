bbce5321b860ce4deee2c4c2eef8cec07661bbb5
Shaig Khaligli <x.shaiq@gmail.com>
2021-01-25 23:14:24 +0100

    code documentation added

--- main/checkout.py
+++ main/checkout.py
@@ -2,28 +2,43 @@ import functools
 
 
 class Checkout(object):
+    """
+        Checkout system
+    """
 
     def __init__(self, promotion_rules=[]):
+        """
+            Initialize checkout class
+            :param promotion_rules:
+        """
         self.promo_rules = promotion_rules
-        self.co_products = []
+        self.co_products = []  # products already scanned
 
+        # check validation of promotion rules and raise error if any
         self.validate_promotion_rules()
 
     def scan(self, product):
+        """
+            Scan product adding to checkout system
+            :param product:
+            :return: bool
+        """
         self.co_products.append(product)
         return True
 
     def subtotal(self):
+        """
+            Subtotal price without
+            applying promotion code
+            :return: int
+        """
         return functools.reduce(lambda sum, cal: sum + cal.price, self.co_products)
 
-
-    def validate_promotion_rules(self):
-        if all([rule.valid() for rule in self.promo_rules]):
-            for rule in self.promo_rules:
-                if not rule.valid():
-                    raise ValueError(f"Promotion {rule.promotion_name} is not valid,\n{rule.error_messages}")
-
     def total(self):
+        """
+            Total price after promotion rule
+            :return: int
+        """
         if not self.promo_rules:
             return self.subtotal()
 
@@ -31,14 +46,28 @@ class Checkout(object):
         promo_rule_for_total = next(rule for rule in self.promo_rules if rule.promotion_type == 'on_total_price')
         total_price_without_promo = self.subtotal()
 
+        # apply promotion code for each item of promo_rules_for_item
         for rule in promo_rules_for_item:
             for product in self.co_products:
                 co_qty = len(list(filter(lambda p: p.product_code == product.product_code, self.co_products)))
                 if rule.applicable(product=product, quantity=co_qty):
                     continue
+                # set promotion price for the product if rule can apply on this product
                 product.price = rule.promotion_price
 
+        # recalculate total price with promotion for item
         total_price = self.subtotal()
+        # apply promotion code for total price of promo_rule_for_total
         if promo_rule_for_total.applicable(total_price=total_price_without_promo):
             total_price -= (promo_rule_for_total.discount_rate / 100 * total_price)
         return round(total_price, 2)
+
+    def validate_promotion_rules(self):
+        """
+            Validate promoting rule
+            :return:
+        """
+        if all([rule.valid() for rule in self.promo_rules]):
+            for rule in self.promo_rules:
+                if not rule.valid():
+                    raise ValueError(f"Promotion {rule.promotion_name} is not valid,\n{rule.error_messages}")
--- main/promotion_rule.py
+++ main/promotion_rule.py
@@ -1,8 +1,13 @@
 class PromotionRule(object):
-
+    """
+        Promotion rule class
+    """
 
     def __init__(self, **kwargs):
-
+        """
+            Initialize PromotionRule class
+            :param kwargs:
+        """
         self.promotion_name = kwargs.get('promotion_name')
         self.promotion_type = kwargs.get('promotion_type')
         self.min_price = kwargs.get('min_price')
@@ -10,16 +15,28 @@ class PromotionRule(object):
         self.promotion_price = kwargs.get('promotion_price')
         self.discount_rate = kwargs.get('discount_rate')
         self.status = kwargs.get('status', 'active')
+        # list product that can applied this promotion if promotion_type is `individual`
         self.products = []
+        self.error_messages = []
 
     def add_product(self, product):
-
+        """
+            Add product
+            :param product:
+            :return: bool
+        """
         self.products.append(product)
         return True
 
-
     def applicable(self, product=None, quantity=0, total_price=0):
-
+        """
+        Check promotion_type
+        on_item_price or on_total_price
+        :param product:
+        :param quantity:
+        :param total_price:
+        :return:
+        """
         if self.promotion_type == 'on_item_price':
             result = []
             for item in self.products:
@@ -30,12 +47,20 @@ class PromotionRule(object):
             return total_price >= self.min_price
 
     def valid(self):
+        """
+            Check validation
+            :return: bool
+        """
         if self.error():
             return False
         else:
             return True
 
     def error(self):
+        """
+            Check error
+            :return:
+        """
         errors = []
         if self.promotion_type == 'on_item_price':
             if not self.products:
@@ -48,3 +73,4 @@ class PromotionRule(object):
 
         self.error_messages = errors
         return errors
+
