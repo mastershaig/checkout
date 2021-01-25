546e5e67a8b0558b4cb4e6a758740c79ce5d04ae
Shaig Khaligli <x.shaiq@gmail.com>
2021-01-25 19:30:51 +0100

    Validate promoting rule

--- main/checkout.py
+++ main/checkout.py
@@ -1,4 +1,27 @@
+import functools
+
+
 class Checkout(object):
-    
-    def __init__(self):
-        pass
+
+    def __init__(self, promotion_rules=[]):
+        self.promo_rules = promotion_rules
+        self.co_products = []
+
+        self.validate_promotion_rules()
+
+    def scan(self, product):
+        self.co_products.append(product)
+        return True
+
+    def subtotal(self):
+        return functools.reduce(lambda sum, cal: sum + cal.price, self.co_products)
+
+
+    def validate_promotion_rules(self):
+        if all([rule.valid() for rule in self.promo_rules]):
+            for rule in self.promo_rules:
+                if not rule.valid():
+                    raise ValueError(f"Promotion {rule.promotion_name} is not valid,\n{rule.error_messages}")
+
+    def total(self):
+    	pass
