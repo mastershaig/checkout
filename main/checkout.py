d0d38d07a75d5140455e3891aedbff554ca1f913
Shaig Khaligli <x.shaiq@gmail.com>
2021-01-25 22:34:50 +0100

    checkout total calculation added

--- main/checkout.py
+++ main/checkout.py
@@ -24,4 +24,21 @@ class Checkout(object):
                     raise ValueError(f"Promotion {rule.promotion_name} is not valid,\n{rule.error_messages}")
 
     def total(self):
-    	pass
+        if not self.promo_rules:
+            return self.subtotal()
+
+        promo_rules_for_item = list(filter(lambda rule: rule.promotion_type == 'on_item_price', self.promo_rules))
+        promo_rule_for_total = next(rule for rule in self.promo_rules if rule.promotion_type == 'on_total_price')
+        total_price_without_promo = self.subtotal()
+
+        for rule in promo_rules_for_item:
+            for product in self.co_products:
+                co_qty = len(list(filter(lambda p: p.product_code == product.product_code, self.co_products)))
+                if rule.applicable(product=product, quantity=co_qty):
+                    continue
+                product.price = rule.promotion_price
+
+        total_price = self.subtotal()
+        if promo_rule_for_total.applicable(total_price=total_price_without_promo):
+            total_price -= (promo_rule_for_total.discount_rate / 100 * total_price)
+        return round(total_price, 2)
